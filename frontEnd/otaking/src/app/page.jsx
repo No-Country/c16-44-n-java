f0a432f449dcf6d0a2c6cc675fc850e2861202f8
Fernando Alcázar Malfavón <4maferal@gmail.com>
2024-02-22 20:07:23 -0600

    feat: add links

--- frontEnd/otaking/src/app/page.jsx
+++ frontEnd/otaking/src/app/page.jsx
@@ -1,8 +1,10 @@
 "use client"
 import GreetingBanner from "@/components/GreetingBanner/GreetingBanner";
+import NavCategory from "@/components/NavCategory/NavCategory";
 
 export default function Home() {
-  return <>
-  <GreetingBanner />
-  </>
+  return <main className="main">
+    <GreetingBanner />
+    <NavCategory />
+  </main>
 }
