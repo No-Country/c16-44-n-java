119b26a75b826de3c3648a233faa2c1a73a35147
Angelina Balatorre <angelinabalatorre@gmail.com>
2024-03-03 22:56:59 -0300

    trying to fetch categories

--- frontEnd/otaking/src/utils/apiClient.js
+++ frontEnd/otaking/src/utils/apiClient.js
@@ -1,7 +1,11 @@
-import {headers, confirm} from './api.js'
+import { headers, confirm } from './api.js'
 
 const user = 'http://localhost:8080/v1/user'
 const product = 'http://localhost:8080/v1/product'
+const name_des = 'http://localhost:8080/v1/product/ordered-by-name-des'
+const name_asc = 'http://localhost:8080/v1/product/ordered-by-name-asc'
+const price_high = 'http://localhost:8080/v1/product/low-to-high'
+const price_low = 'http://localhost:8080/v1/product/high-to-low'
 
 const setPostOpt = (form) => ({
   method: 'POST',
@@ -10,7 +14,7 @@ const setPostOpt = (form) => ({
 })
 
 export const getAllUsers = () => {
-  return fetch('/api', {headers})
+  return fetch('/api', { headers })
     .then(confirm)
 }
 
@@ -32,6 +36,32 @@ export const login = (form) => {
 }
 
 export const getCategory = (category) => {
-  return fetch(product + '/category/' + category, {headers, next: {revalidate: 15}})
+  return fetch(product + '/category/' + category, { headers, next: { revalidate: 15 } })
+    .then(confirm)
+}
+
+export const getCategoryDes = async () => {
+  try {
+    const response = await fetch(name_des, { headers });
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return null;
+  }
+};
+
+export const getCategoryAsc = () => {
+  return fetch(`${name_asc}`, { headers, next: { revalidate: 15 } })
+    .then(confirm)
+}
+
+export const getCategoryHigh = () => {
+  return fetch(`${price_high}`, { headers, next: { revalidate: 15 } })
+    .then(confirm)
+}
+
+export const getCategoryLow = () => {
+  return fetch(`${price_low}`, { headers, next: { revalidate: 15 } })
     .then(confirm)
 }
