bd9450fb981dcc60f153d288e7f73f49e7154a8f
Gonzalo Villegas <gvillegas1276@hotmail.com>
2024-03-04 21:27:17 -0500

    update branch main

--- backEnd/src/main/java/com/marketplace/usersservice/repository/ProductCartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.marketplace.usersservice.repository;
-
-import com.marketplace.usersservice.model.ProductCart;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductCartRepository extends JpaRepository<ProductCart, Long> {
-}
