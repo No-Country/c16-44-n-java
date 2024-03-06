d4553c5c1283d901b048ee318d5b9ebeb383a7fe
Gonzalo Villegas <gvillegas1276@hotmail.com>
2024-03-06 12:13:46 -0500

    update properties

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
 -- Modifications To user_type
--- INSERT INTO user_type (user_type) VALUES ('USER'), ('ADMIN'), ('SELLER');
+INSERT INTO user_type (user_type) VALUES ('USER'), ('ADMIN'), ('SELLER');
 -- Poner el usuario que va a conectarse a la base de datos
 -- REVOKE INSERT, UPDATE, DELETE ON user_type FROM 'username'@'hostname';
 
