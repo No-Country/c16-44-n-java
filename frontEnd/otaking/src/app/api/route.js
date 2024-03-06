7dd91d64acd18dbf42ee7eada555dc31870c39c0
Angelina Balatorre <angelinabalatorre@gmail.com>
2024-03-06 18:35:54 -0300

    uploading fetch url

--- frontEnd/otaking/src/app/api/route.js
+++ frontEnd/otaking/src/app/api/route.js
@@ -1,5 +1,5 @@
 import { confirm, headers, res } from '@/utils/api.js'
-export const BASE_URL = 'http://localhost:8080/v1/user'
+export const BASE_URL = 'https://c16-44-n-java.onrender.com/v1/user'
 
 const postUser = (res) => {
   return fetch(BASE_URL + '/create', {
@@ -7,14 +7,14 @@ const postUser = (res) => {
     headers,
     body: JSON.stringify(res)
   })
-  .then(confirm)
+    .then(confirm)
 }
 
 export function GET() {
   return fetch(BASE_URL + '/all', {
     headers,
   }).then(confirm)
-  .then(res)
+    .then(res)
 }
 
 export function POST(req) {
